5c6a4fa60580f07d3197d373aaa6d1cd7d676ebc
benoah <noahbenaas@gmail.com>
2020-08-24 18:10:02 +0200

    Add files via upload

---
+++ build/precache-manifest.87f3997258bf930fe90ff5c0ebbf4ce4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce393ff818699d71557c84db491cbd43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "875a5136c225ef9c72e1",
+    "url": "/static/css/main.a2ca506e.chunk.css"
+  },
+  {
+    "revision": "4591939277bc644d5379",
+    "url": "/static/js/2.4e424880.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e424880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875a5136c225ef9c72e1",
+    "url": "/static/js/main.4280e823.chunk.js"
+  },
+  {
+    "revision": "60b55fea15a920958983",
+    "url": "/static/js/runtime-main.9c1b8abd.js"
+  }
+]);
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <footer>
+            this is the footer 
+        </footer>
+
+    )
+}
+
+export default Footer;
+
